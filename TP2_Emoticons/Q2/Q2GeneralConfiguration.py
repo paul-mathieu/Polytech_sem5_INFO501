c4e102592d644b251c35b1f8c4b56c36673d04f6
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-15 09:57:21 +0100

    Update Q2GeneralConfiguration.py

--- TP2_Emoticons/Q2/Q2GeneralConfiguration.py
+++ TP2_Emoticons/Q2/Q2GeneralConfiguration.py
@@ -8,44 +8,111 @@ Created on Thu Nov 16 19:47:50 2017
 import pygame
 
 class GeneralConfiguration:
+    
+    #=============================================================================
+    #=============================================================================
+    # Part I - Initialisation
+    #=============================================================================
+    #=============================================================================
+    
+    
+    #==========================================================================
     # Constructor
+    #==========================================================================    
     def __init__(self) :
+        
+        self.screenWidth = 1000
+        self.screenHeight = 1200
+        
         self.initPygame()
         
         # Parameters for the emoticons        
-        self.emoticonSize = 400
-        self.emoticonBorder = 20  
+        self.emoticonSize = 500
+        self.emoticonBorder = 200 
         
         # Parameters for the buttons
         self.buttonWidth = 150
         self.buttonHeight = 80
+        
 
+
+    #==========================================================================
     # Initializes pygame
-    def initPygame(self): 
+    #==========================================================================
+    def initPygame(self):
+        
         #Initialization
         pygame.init()
         # Sets the screen size.
-        pygame.display.set_mode((800, 600))    
+        pygame.display.set_mode((self.screenWidth, self.screenHeight))    
         # Sets the timer to check event every 200 ms
         pygame.time.set_timer(pygame.USEREVENT, 200)
         # Gets pygame screen
-        self.screen = pygame.display.get_surface()         
+        self.screen = pygame.display.get_surface()       
         
     # Getters
     # Compléter avec les Getters de la question Q1.4
 
-        
+
+
+    #=============================================================================
+    #=============================================================================
+    # Part II - Display
+    #=============================================================================
+    #=============================================================================
     # Draws on pygame screen      
     def draw(self):
         pass
             
+
+
+
+        
+    #==========================================================================
     # Displays   
+    #==========================================================================
     def display(self):
-        # Draws on the screen surface
-        self.draw()
+        
+        #•Tritre de la fenêtre
+        pygame.display.set_caption("Mesure de la Température")
+        
+    
+        
         
         # Updates the displat and clears new timer events
         pygame.display.flip()
         pygame.event.clear(pygame.USEREVENT)
-               
+        
+    #Taille de l'emoticonne dans un carre
 
+    
+    
+    
+    
+    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        

0f871214945e9ac57f93626e781cf2a4e0b2c391
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-15 09:57:19 +0100

    Update Q2Emoticon.py

--- TP2_Emoticons/Q2/Q2Emoticon.py
+++ TP2_Emoticons/Q2/Q2Emoticon.py
@@ -10,13 +10,93 @@ import math
 
 class Emoticon:
 
-    # Setters
+    #=============================================================================
+    #=============================================================================
+    # Part I - Initialisation
+    #=============================================================================
+    #=============================================================================
+    
+    
+    #==========================================================================
+    # Constructor
+    #==========================================================================    
     def setGeneralConfiguration(self, generalConfiguration):
         self.generalConfiguration = generalConfiguration
 
-    # Draws the emoticon    
-    def draw(self):   
-        pass
+
+
+    #==========================================================================
+    # Constructor
+    #==========================================================================    
+    def headToArea(self):
+        
+        #position du centre du nouveau carre
+        x = self.generalConfiguration.screenWidth / 2
+        y = self.generalConfiguration.buttonHeight + self.generalConfiguration.emoticonBorder + self.generalConfiguration.emoticonSize / 2
+
+        
+        return x, y
+
+
+
+    #=============================================================================
+    #=============================================================================
+    # Part II - Draw
+    #=============================================================================
+    #=============================================================================
+
+ 
+    #==========================================================================
+    # Draws on pygame screen      
+    #==========================================================================
+    def draw(self):
+        
+        #for the buttons
+        self.printSquareButtons()
+        
+        #for the emoticon
+        self.printSquareEmoticon()        
+    
+    #==========================================================================
+    #Square for the emoticon
+    #==========================================================================
+    def printSquareEmoticon(self):
+        
+        #carre pour l'emoticonne
+        widthRect = self.generalConfiguration.emoticonSize
+        heightRect = self.generalConfiguration.emoticonSize
+        x = self.generalConfiguration.screenWidth / 2 - widthRect / 2
+        y = self.generalConfiguration.buttonHeight + self.generalConfiguration.emoticonBorder
+
+        pygame.draw.rect(self.generalConfiguration.screen, (255,255,255), (x, y, widthRect, heightRect),1  )
+        
+        #dessin de l'emoticonne
+        #self.draw()    
+
+
+
+    #=============================================================================
+    #=============================================================================
+    # Part III - Buttons
+    #=============================================================================
+    #=============================================================================
+
+
+        
+    #==========================================================================
+    # 
+    #==========================================================================
+
+    def printSquareButtons(self):
+        
+        #carre pour l'emoticonne
+        widthRect = self.generalConfiguration.screenWidth
+        heightRect = self.generalConfiguration.buttonHeight
+        x = 0
+        y = 0
+
+        pygame.draw.rect(self.generalConfiguration.screen, (255,255,255), (x, y, widthRect, heightRect),1  )
+
 
 
 

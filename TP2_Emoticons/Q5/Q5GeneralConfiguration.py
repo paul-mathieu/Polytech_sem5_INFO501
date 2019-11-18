41d50931fb940a9b97a5299dc82f0556b320e5c5
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-18 12:05:36 +0100

    Update Q5GeneralConfiguration.py

--- TP2_Emoticons/Q5/Q5GeneralConfiguration.py
+++ TP2_Emoticons/Q5/Q5GeneralConfiguration.py
@@ -10,7 +10,12 @@ from Q5Emoticon import Emoticon
 from Q5Button import Button
 
 class GeneralConfiguration:
+    
+    
+    #==========================================================================
     # Constructor
+    #==========================================================================    
+    
     def __init__(self) :
         self.initPygame()
         
@@ -26,8 +31,12 @@ class GeneralConfiguration:
         self.sensors = []
         
         self.selectedSensor = 0
+
         
-    # Initializes pygame        
+    #==========================================================================
+    # Initializes pygame
+    #==========================================================================    
+    
     def initPygame(self): 
         #Initialization
         pygame.init()
@@ -38,34 +47,83 @@ class GeneralConfiguration:
         # Gets pygame screen
         self.screen = pygame.display.get_surface()         
         
-    # Getters
-    # Compléter avec les Getters de la question Q1.4
+  
+      
+    #==========================================================================
+    # Getters 
+    #==========================================================================    
 
+    def getScreen(self):
+        return self.screen
+
+    def getEmoticonSize(self):
+        return self.emoticonSize
+
+    def getEmoticonBorder(self):
+        return self.emoticonBorder
+
+    def getButtonWidth(self):
+        return self.buttonWidth
+
+    def getButtonHeight(self):
+        return self.buttonHeight
+
+    
+    def getSensors(self):
+        return self.sensors
         
-    # Adds a sensor    
+    def getSelectedSensor(self):
+        return self.selectedSensor
+        
+        
+        
+    #==========================================================================
+    # Adds a sensor 
+    #==========================================================================    
+
     def addSensor(self, sensor):
         sensor.setGeneralConfiguration(self)
         sensor.setSensorId(len(self.sensors))
         sensor.setEmoticon(Emoticon(sensor))
         sensor.setButton(Button(sensor))
         self.sensors.append(sensor)
+
+
  
+    #==========================================================================
     # Retrieves the sensor id from a posiiion
+    #==========================================================================    
+
     def positionToSensorId(self, position):
         pass
 
+
+
+    #==========================================================================
     # Checks if the display of a new sensor was requested
+    #==========================================================================    
+
     def checkIfSensorChanged(self, eventPosition):
         pass
+
+
     
-    # Draws on pygame screen      
+    #==========================================================================
+    # Draws on pygame screen   
+    #==========================================================================    
+
     def draw(self):
         # Clears the surface
         pygame.display.get_surface().fill([0, 0, 0])
         
         pass
+ 
+
             
-    # Displays   
+    #==========================================================================
+    # Displays
+    #==========================================================================    
+
     def display(self):
         # Draws on the screen surface
         self.draw()
@@ -73,6 +131,8 @@ class GeneralConfiguration:
         # Updates the display and clears new timer events
         pygame.display.flip()
         pygame.event.clear(pygame.USEREVENT)
+
+
 
 
 

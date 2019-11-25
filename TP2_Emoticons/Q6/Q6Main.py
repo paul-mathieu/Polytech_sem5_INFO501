32f295d329d46d5e85e71e821ea849f0b3baf93b
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-25 12:07:58 +0100

    Update Q6Main.py

--- TP2_Emoticons/Q6/Q6Main.py
+++ TP2_Emoticons/Q6/Q6Main.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Nov 16 19:47:50 2017
 
-@author: lfoul
+@author: FANTON - MATHIEU
 """
 
 import pygame
@@ -10,15 +10,6 @@ from Q6GeneralConfiguration import GeneralConfiguration
 from Q6Sensor import Sensor
              
 def main():
-    
-    
-    
-#    listeX = [- x / 10 for x in range(10)][::-1] + [x / 10 for x in range(10)] 
-#    listeX += listeX[::-1] 
-#    listeX *= 100
-    
-    
-    
  
     # Creates the general configuration and the sensors
     initURL = 'https://www.polytech.univ-smb.fr/apps/myreader/capteur.php?capteur='
@@ -30,13 +21,17 @@ def main():
     generalConfiguration.addSensor(Sensor(initURL + 'epua_toiture', 'Temp. Toiture B204', [30, 35, 40]))
     generalConfiguration.addSensor(Sensor(initURL + 'epua_onduleur1_watts', 'Puiss. Onduleur', [10000, 12000, 15000]))
     
+    generalConfiguration.addSensor(Sensor(initURL + 'epua_b204_clim', 'Temp. Clim B204', [20, 22, 23]))
+    generalConfiguration.addSensor(Sensor(initURL + 'epua_b204_coursive', 'Temp. Coursive B204', [20, 22, 23]))
+    generalConfiguration.addSensor(Sensor(initURL + 'epua_b204_centre', 'Temp. Centre B204', [20, 22, 23]))
+    generalConfiguration.addSensor(Sensor(initURL + 'epua_toiture', 'Temp. Toiture B204', [30, 35, 40]))
+
+    
 #    print(str(generalConfiguration.sensors))
 
-#    print('a')
 
     # Infinite loop    
     while True:
-#    for x in range(2000):
 
         # Waits for an event
         event = pygame.event.wait()
@@ -50,11 +45,13 @@ def main():
             
             generalConfiguration.draw()
             
+            print(str(generalConfiguration.selectedSensor))
+            
             generalConfiguration.display()
             
 #            print(str(generalConfiguration.positionToSensorId(pygame.mouse.get_pos())))         
 #            print(str(generalConfiguration.maxButtonsPerLine()))
-            print(str(generalConfiguration.buttonsCountOnLine(1)))
+#            print(str(generalConfiguration.buttonsCountOnLine(1)))
             
         elif event.type == pygame.MOUSEBUTTONDOWN:
             
